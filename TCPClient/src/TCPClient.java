635d2100af95580c694f1407d29dc61fe50f39fb
joao <ja.teixeira@campus.fct.unl.pt>
2020-11-02 08:58:33 +0000

    Updated Stunnel (Cleaner conf)

--- TCPClient/src/TCPClient.java
+++ TCPClient/src/TCPClient.java
@@ -7,15 +7,15 @@ import java.net.Socket;
 public class TCPClient {
     public static final int SERVER_PORT = 1234;
     public static final int CLIENT_PORT = 1237;
-    public static final String LOCALHOST = "172.28.0.4";
-    public static final String SERVERHOST = "172.28.0.5";
+    public static final String LOCALHOST = "localhost";  //"172.28.0.4";
+    public static final String SERVERHOST = "localhost"; //"172.28.0.5";
     public static final String QUIT = "quit";
 
     public static void main(String[] argv) throws Exception {
         String clientSentence = "";
         String serverSentence;
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        Socket clientSocket = new Socket(SERVERHOST, SERVER_PORT, InetAddress.getByName(LOCALHOST), CLIENT_PORT);
+        Socket clientSocket = new Socket(SERVERHOST, SERVER_PORT);
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         while (!clientSentence.equalsIgnoreCase(QUIT)) {
